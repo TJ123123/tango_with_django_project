feb24e53dc4f964ae5a84bb79fc60b345c2dda78
~ Geddes ~ <2461474G@student.gla.ac.uk>
2021-02-02 12:29:27 +0000

    end of 5 (but working)

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,8 +23,9 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
+    path('', views.index, name='index'),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
