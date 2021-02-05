2296e94e8eb3a16cb7b30055a85ffbe02ded8d88
~ Geddes ~ <2461474G@student.gla.ac.uk>
2021-02-05 15:59:38 +0000

    end of 8- all working

--- rango/views.py
+++ rango/views.py
@@ -27,8 +27,9 @@ def index(request):
 
 
 def about(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/about.html', context=context_dict)
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html')
 
 
 def show_category(request, category_name_slug):
